67404e47b1e77a8a154a941f9964500b2d61ec40
roque jesus potenza sanchez <roquepotenza@hotmail.com>
2021-09-18 12:12:15 -0430

    se exportaron variables

--- practica_curso.js
+++ practica_curso.js
@@ -1,21 +1,54 @@
-const datosPersonales=[{
-"nombres":"roque jesus",
-"apellidos":"potenza sanchez",
-"edad":"36",
-"fechaNacimiento":"09/01/1985",
-"telefono":"0424-3503227",
-"direccion":"venezuela",
-"peliculas":["matrix","volver al futuro","jumanji","jurasic park","rapidos y furiosos","sheldon"]
-},{
-    "nombres":"marcos jesus",
-    "apellidos":"perez sanchez",
-    "edad":"39",
-    "fechaNacimiento":"09/01/1981",
-    "telefono":"0424-15203427",
-    "direccion":"venezuela",
-    "peliculas":["matrix","volver al futuro","jumanji","jurasic park","rapidos y furiosos","sheldon"]
-    }];
+const datosPersonales = [
+  {
+    nombres: 'roque jesus',
+    apellidos: 'potenza sanchez',
+    edad: '36',
+    fechaNacimiento: '09/01/1985',
+    telefono: '0424-000000',
+    direccion: 'venezuela',
+    peliculas: [
+      'matrix',
+      'volver al futuro',
+      'jumanji',
+      'jurasic park',
+      'rapidos y furiosos',
+      'sheldon'
+    ]
+  },
+  {
+    nombres: 'marcos jesus',
+    apellidos: 'perez sanchez',
+    edad: '39',
+    fechaNacimiento: '09/01/1981',
+    telefono: '0424-000000',
+    direccion: 'venezuela',
+    peliculas: [
+      'matrix',
+      'volver al futuro',
+      'jumanji',
+      'jurasic park',
+      'rapidos y furiosos',
+      'sheldon'
+    ]
+  }
+];
 
+const temp1 = 'Este es un texto ficticio1';
+const temp2 = 'Este es un texto ficticio2';
+let datePerson = {
+  Nombre: 'roque',
+  Apellido: 'Potenza'
+};
+let cadenaStringLiteral = `Mi Nombre: ${datePerson.Nombre} y mi apellido es: ${datePerson.Apellido}`;
+
+function rest(...parametros) {
+  return parametros;
+}
+
+let parametrosRest = rest(1, 2, 3, 4, 5);
+
+export { temp1, temp2, datosPersonales, parametrosRest, cadenaStringLiteral };
+/**/
 /*Desestructuracion
 
 let {nombres, apellidos, direccion, peliculas}=datosPersonales;
@@ -26,8 +59,8 @@ for(let i=0; i<peliculas.length; i++){
     console.log(peliculas[i]);
 }
 
-*/
-for(i=0; i<datosPersonales.length;i++){
+
+for(let i=0; i<datosPersonales.length;i++){
     console.log("Mi nombre es: "+datosPersonales[i].nombres+" y mi apellidos es: "+datosPersonales[i].apellidos+"y vivo en:"+datosPersonales[i].direccion);
     console.log("Mis Peliculas Preferidas es:"); 
     for(let j=0; j<datosPersonales[i].peliculas.length; j++){
@@ -35,6 +68,4 @@ for(i=0; i<datosPersonales.length;i++){
         console.log(datosPersonales[i].peliculas[j]);
     }
 }
-
-
-
+*/
